7f15640d43667e13d1ff2b7d28f623afe6265e86
Carlos Morales <cmorales@sie.gob.gt>
2021-04-09 15:11:05 -0600

    Bases del proyecto Tareas

---
+++ 04-Tareas/helpers/mensajes.js
@@ -0,0 +1,47 @@
+const { red } = require('colors');
+
+require('colors');
+
+const mostrarMenu = () => {
+
+    console.clear();
+    console.log('============================='.rainbow);
+    console.log('      Seleccione Opcion '.rainbow)
+    console.log('=============================\n'.rainbow)
+
+    console.log(`${'1. '.green} Crear Tarea:`);
+    console.log(`${'2. '.green} Editar Tareas:`);
+    console.log(`${'3. '.green} Listar Tareas Completadas:`);
+    console.log(`${'4. '.green} Listar Tareas Pendientes:`);
+    console.log(`${'5. '.green} Completar tarea(s):`);
+    console.log(`${'6. '.green} Borrar Tarea:`);
+    console.log(`${'7. '.green} Salir \n`);
+
+    const readline = require('readline').createInterface({   //se realiza una funcion para recibir datos del usuario
+        input: process.stdin,  // entrada 
+        output: process.stdout //salida
+    });
+
+    readline.question('Seleccione una upcion: ', (opt) => { // el opt es la respuesta que espera el usuario pero en un callback
+        //console.log({opt})
+        readline.close();
+    })
+
+}
+
+const pausa = () => {
+
+    const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+    })
+
+    readline.question(`\n Presione ${'Enter'.green} para continuar ... `, (opt) => {
+            readline.close();
+     })
+    }
+
+module.exports = {
+    mostrarMenu, 
+    pausa
+}
