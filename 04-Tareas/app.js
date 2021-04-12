39c958fa5bb7a9089bce366cd4e4d59a023513d7
Carlosl2 <carlosmorales_lopez@hotmail.com>
2021-04-12 12:03:04 -0600

    Menu Infinito 04-Tareas

--- 04-Tareas/app.js
+++ 04-Tareas/app.js
@@ -4,8 +4,20 @@ const {mostrarMenu, pausa} = require('./helpers/mensajes')
 
 const main = async () => 
 {
-    mostrarMenu(); 
-    // pausa(); 
+    let opt =''; 
+    do{
+        opt = await mostrarMenu(); 
+        console.log({opt}); 
+
+        if(opt !=='0')
+        {
+            await pausa();
+        }
+       
+
+    }while (opt !== '0')
+    
+
 }
 
 main();
